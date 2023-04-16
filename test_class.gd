b8e913e0550aab73dd92d8a813f39ca263ea7337
jmacc93 <joe_miller_93@protonmail.com>
2023-04-16 09:28:29 -0500

    Moving files

--- test_class.gd
+++
@@ -1,9 +0,0 @@
-extends Node
-
-class_name TestClass
-
-signal siga
-
-# Called when the node enters the scene tree for the first time.
-func _ready():
-  add_user_signal("sigb")
--- textWithCustomFont.gd
+++
@@ -1,7 +0,0 @@
-extends Label
-
-@export var font_resource : Font
-
-func _init():
-  add_theme_font_override("normal_font", font_resource)
-#  font = font_resource
